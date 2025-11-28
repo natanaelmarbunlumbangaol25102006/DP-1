870c52c626e8cd0371178403f7e29779894d22c9
natanaelmarbunlumbangaol25102006 <natanaelmarbunlumbangaol@gmail.com>
2025-11-28 12:06:12 +0700

    Pertemuan 8 : Pointer

---
+++ Pertemuan 8/tukarPointer.pas
@@ -0,0 +1,23 @@
+program tukarPointer;
+uses crt;
+
+var 
+    rumah : string[20];
+    peta : ^string;
+
+begin 
+    clrscr;
+    rumah := 'Rumah Joshua';
+    writeln('Variabel rumah berisi : ', rumah);
+
+    {pointer (maps) berisi alamat rumah, kita coba tampilkan
+    isi dari alamat tsb}
+    peta := @rumah;
+    writeln('Variabel peta menunjuk ke rumah: ', peta^);
+
+    {isi dari 'rumah' akan kita ubah lewat alamat}
+    writeln;
+    writeln('Rumah Joshua di gusur Aini !!');
+    peta^ := 'Rumah Aini';
+    writeln('Variabel rumah sekarang berisi : ', rumah);
+end.
